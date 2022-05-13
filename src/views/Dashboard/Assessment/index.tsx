628494498cf189ad4b15c86700d8434eb7595de2
Tobias Leinss <caruso33@web.de>
2022-05-13 14:10:07 +0200

    Add get controller, gardens

--- src/views/Dashboard/Assessment/index.tsx
+++ src/views/Dashboard/Assessment/index.tsx
@@ -3,35 +3,38 @@ import { useWeb3React } from "@web3-react/core";
 import { useHistory } from "react-router-dom";
 import { ethers } from "ethers";
 import IBabController from "./IBabController.json";
+import { useState } from "react";
 
 export default function Assessment() {
   const { active, library } = useWeb3React();
   const history = useHistory();
+  const [controller, setController] = useState<null | ethers.Contract>(null);
 
   function getControllerContract() {
     const contractAddress = "0xD4a5b5fcB561dAF3aDF86F8477555B92FBa43b5F";
 
+    const signer = library.getSigner();
+
     const controllerContract = new ethers.Contract(
       contractAddress,
       IBabController.abi,
-      library.provider
+      signer
     );
+
+    console.log({ controllerContract });
+    setController(controllerContract);
 
     return controllerContract;
   }
 
+  async function getGardens() {
+    const gardens = await controller!.getGardens();
+
+    console.log({ gardens });
+  }
+
   return (
     <VStack justifyContent="center" alignItems="center" h="100vh">
-      <HStack>
-        <Button
-          onClick={() => {
-            getControllerContract();
-          }}
-        >
-          Get Controller Contract
-        </Button>
-      </HStack>
-
       <HStack>
         {!active ? (
           <Button onClick={() => history.push("/admin/connect")}>
@@ -40,6 +43,18 @@ export default function Assessment() {
         ) : (
           <Text>Connected</Text>
         )}
+      </HStack>
+
+      <HStack>
+        <Button onClick={getControllerContract} disabled={!active}>
+          Get Controller Contract
+        </Button>
+      </HStack>
+
+      <HStack>
+        <Button onClick={getGardens} disabled={!active || !controller}>
+          Get Gardens
+        </Button>
       </HStack>
     </VStack>
   );

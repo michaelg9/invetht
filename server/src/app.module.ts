de36ffc6cc3c3c86b3f134c356958c3e2a751792
Tobias Leinss <caruso33@web.de>
2022-07-12 20:15:56 +0200

    Add mongo

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import 'dotenv/config';
+
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_pw = process.env.MONGODB_PW;
+const mongodb_suffix = process.env.MONGODB_SUFFIX;
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    MongooseModule.forRoot(
+      `mongodb+srv://${mongodb_user}:${mongodb_pw}@${mongodb_suffix}`,
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
